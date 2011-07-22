a394e8423453240d83e1fdbb90eaf5954dda0b67
mausch <mauricioscheffer@gmail.com>
2011-07-22 18:00:34 -0300

    comments, xhtml namespace

--- MiniMVC/X.cs
+++ MiniMVC/X.cs
@@ -3,6 +3,9 @@ using System.Linq;
 using System.Xml.Linq;
 
 namespace MiniMVC {
+    /// <summary>
+    /// Various helper functions to deal with XML / XHTML / HTML
+    /// </summary>
     public static class X {
         /// <summary>
         /// Shortcut to build a <see cref="XAttribute"/>
@@ -41,19 +44,54 @@ namespace MiniMVC {
             return e;
         }
 
+        /// <summary>
+        /// XHTML 1.0 Transitional doctype
+        /// </summary>
         public static readonly XDocumentType XHTML1_0_Transitional = new XDocumentType("html", "-//W3C//DTD XHTML 1.0 Transitional//EN", "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd", null);
+
+        /// <summary>
+        /// XHTML 1.0 Strict doctype
+        /// </summary>
         public static readonly XDocumentType XHTML1_0_Strict = new XDocumentType("html", "-//W3C//DTD XHTML 1.0 Strict//EN", "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd", null);
+
+        /// <summary>
+        /// HTML 5 doctype
+        /// </summary>
         public static readonly XDocumentType HTML5 = new XDocumentType("html", null, null, null);
 
+        /// <summary>
+        /// Non-breaking space
+        /// </summary>
         public static readonly string nbsp = "\u00A0";
+
+        /// <summary>
+        /// Right angle quotation mark
+        /// </summary>
         public static readonly string raquo = "\u00BB";
+
+        /// <summary>
+        /// Left angle quotation mark
+        /// </summary>
         public static readonly string laquo = "\u00AB";
+
+        /// <summary>
+        /// Single right-pointing angle quotation mark
+        /// </summary>
         public static readonly string rsaquo = "\u203A";
+
+        /// <summary>
+        /// Single left-pointing angle quotation mark
+        /// </summary>
         public static readonly string lsaquo = "\u2039";
 
         /// <summary>
         /// Copyright character entity
         /// </summary>
         public static readonly string copy = "\u00A9";
+
+        /// <summary>
+        /// XHTML namespace (http://www.w3.org/1999/xhtml)
+        /// </summary>
+        public static readonly XNamespace XHTML_Namespace = XNamespace.Get("http://www.w3.org/1999/xhtml");
     }
 }
