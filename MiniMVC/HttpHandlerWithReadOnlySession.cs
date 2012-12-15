e3f9c25086146052db80acba0875b4e4dd38a6ba
mausch <mauricioscheffer@gmail.com>
2012-12-15 20:51:17 -0300

    Remove duplicate code

--- MiniMVC/HttpHandlerWithReadOnlySession.cs
+++ MiniMVC/HttpHandlerWithReadOnlySession.cs
@@ -3,19 +3,7 @@ using System.Web;
 using System.Web.SessionState;
 
 namespace MiniMVC {
-    public class HttpHandlerWithReadOnlySession : IHttpHandler, IReadOnlySessionState {
-        private readonly Action<HttpContextBase> action;
-
-        public HttpHandlerWithReadOnlySession(Action<HttpContextBase> action) {
-            this.action = action;
-        }
-
-        public bool IsReusable {
-            get { return true; }
-        }
-
-        public void ProcessRequest(HttpContext context) {
-            action(new HttpContextWrapper(context));
-        }
+    public class HttpHandlerWithReadOnlySession : HttpHandler, IReadOnlySessionState {
+        public HttpHandlerWithReadOnlySession(Action<HttpContextBase> action) : base(action) {}
     }
 }
--- MiniMVC/HttpHandlerWithSession.cs
+++ MiniMVC/HttpHandlerWithSession.cs
@@ -3,19 +3,7 @@ using System.Web;
 using System.Web.SessionState;
 
 namespace MiniMVC {
-    public class HttpHandlerWithSession: IHttpHandler, IRequiresSessionState {
-        private readonly Action<HttpContextBase> action;
-
-        public HttpHandlerWithSession(Action<HttpContextBase> action) {
-            this.action = action;
-        }
-
-        public bool IsReusable {
-            get { return true; }
-        }
-
-        public void ProcessRequest(HttpContext context) {
-            action(new HttpContextWrapper(context));
-        }
+    public class HttpHandlerWithSession: HttpHandler, IRequiresSessionState {
+        public HttpHandlerWithSession(Action<HttpContextBase> action) : base(action) {}
     }
 }
